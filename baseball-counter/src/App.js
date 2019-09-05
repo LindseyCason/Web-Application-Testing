65fd1b34387bd4433da996b742f3e1ee5e57fbd2
Lindsey Cason <lindseyacason@gmail.com>
2019-09-05 18:16:57 -0500

    calculations updated

--- baseball-counter/src/App.js
+++ baseball-counter/src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 import Dashboard from "./components/dashboard"
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Dashboard />
+       
       </header>
     </div>
   );
--- baseball-counter/src/components/dashboard.jsx
+++ baseball-counter/src/components/dashboard.jsx
@@ -6,10 +6,9 @@ const Dashboard = ()=>{
     const [ballCount, setBallCount] = useState(0);
     const [strikeCount, setStrikeCount] = useState(0);
     const [foulCount, setFoulCount] = useState(0);
-    const [hitCount, setHitCount] = useState(0);
 
 
-    if(ballCount >= 3){
+    if(ballCount > 3){
      setBallCount(0);
     }
 
@@ -17,24 +16,38 @@ const Dashboard = ()=>{
         setStrikeCount(0);
     }
 
-    if(foulCount > 3){
-        setFoulCount(0)
+    const foul = () =>{
+
+        if(foulCount <= 2 ){
+            setStrikeCount(strikeCount + 1)
+        }
+        if(strikeCount === 1){
+            setStrikeCount(2);
+        }
+        if(strikeCount === 2){
+            setStrikeCount(2)
+        }
+
     }
 
-    if(hitCount > 1){
-        setHitCount(hitCount);
-        // setStrikeCount(0) &&
-        // setFoulCount(0);
+
+    const reset = () => {
+        setBallCount(0);
+        setFoulCount(0);
+        setStrikeCount(0);
     }
  
 
 return(
 <div className="buttons">
-    <button onClick={()=> setBallCount(ballCount + 1)} className="BallButton">Ball Test {ballCount}</button>
-    <button onClick={()=> setStrikeCount(strikeCount + 1)} className="StrikeButton">Strike Test {strikeCount}</button>
-    <button onClick={()=> setFoulCount(foulCount + 1)} className="FoulButton">Foul Test {foulCount}</button>
-    <button onClick={()=> setHitCount(hitCount + 1)} className="HitButton">Hit Test {hitCount}</button>
+    <button onClick={()=> setBallCount(ballCount + 1)} className="BallButton">Ball</button> {ballCount}
+    <button onClick={()=> setStrikeCount(strikeCount + 1)} className="StrikeButton">Strike</button> {strikeCount}
+    <button onClick={()=> (foul())} className="FoulButton">Foul</button>
+    <button onClick={()=> (reset())} className="HitButton">Hit</button>
 </div>
+
+
+
 )
     };
 
